0b614404d3f3910f947448f0c32b14da4722b10a
Luiz Pedro Corrêa <luizpedrocorrea98@gmail.com>
2024-08-19 20:02:24 -0300

    add: basic project files

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExemploFundamentos.Models;
+
+namespace ExemploFundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            
+            pessoa1.Nome = "Luiz Pedro";
+            pessoa1.Idade = 26;
+
+            pessoa1.Apresentar();
+        }
+    }
+}
